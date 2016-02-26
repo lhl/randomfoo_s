43a301207125cafbc21763609fe683c1231473a8
lhl <lhl@randomfoo.net>
2016-02-26 15:37:30 -0500

    SoundManager

--- footer.php
+++ footer.php
@@ -15,14 +15,14 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'randomfoo_s' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'randomfoo_s' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'randomfoo_s' ), 'randomfoo_s', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+
+<script type="text/javascript" src="/music/soundmanager/script/soundmanager2-nodebug-jsmin.js"></script>
+<script type="text/javascript" src="/music/inlineplayer.js"></script>
 
 </body>
 </html>
